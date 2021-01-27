0e5d26bc3c11ddd83abb6f692beee06595fca99b
Ted <TheodoreCooke@trentu.ca>
2021-01-27 01:25:06 -0500

    initial commit

---
+++ content.js
@@ -0,0 +1,16 @@
+//var firstHref = $("a[href^='http']").eq(0).attr["href"]
+
+var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+alert("Hello from your script " + firstHref)
+
+// content.js
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      alert(firstHref);
+    }
+  }
+);
