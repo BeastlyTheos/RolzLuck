786b04c13ef651f3bf4ed09875589c7812b5c42b
Ted <TheodoreCooke@trentu.ca>
2021-01-27 12:39:22 -0500

    reports all content errors as alerts

--- content.js
+++ content.js
@@ -1,17 +1,18 @@
-//var firstHref = $("a[href^='http']").eq(0).attr["href"]
+//var firstHref = $("a[href^='http']").eq(0).attr("href");
 
-var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-alert("Hello from your script " + firstHref)
+//alert("Hello from your script " + firstHref)
 
 // content.js
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
+try {
     if( request.message === "clicked_browser_action" ) {
       var firstHref = $("a[href^='http']").eq(0).attr("href");
 
       alert(firstHref);
       chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
     }
+}// end try
+catch (err) {alert("error! " + err);}
   }
 );
