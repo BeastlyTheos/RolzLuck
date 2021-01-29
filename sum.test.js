a1a09efe7d350584aaab64fbfd2a94076c48520e
Ted <TheodoreCooke@trentu.ca>
2021-01-29 04:08:31 -0500

    stylise code commited a couple commits ago

--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,7 @@
-const sum = require("./sum") 
-test(
-	"adds 1 and 2 to equal 3",
-	()=>{
-		expect(sum(1,2)).toBe(3)
-	}
-)
+const sum = require("./sum")
+            test(
+                "adds 1 and 2 to equal 3",
+()=> {
+	expect(sum(1,2)).toBe(3)
+}
+            )
