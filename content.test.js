81e472a3577f00f98289339a70fae5604ebbf80a
Ted <TheodoreCooke@trentu.ca>
2021-01-29 07:30:40 -0500

    have a test that passes!

---
+++ content.test.js
@@ -0,0 +1,14 @@
+const content = require("./content")
+
+class MockMutation {
+	constructor(type) {
+		this.type = type
+	}
+}
+
+test(
+"isNewMessageMutation returns true when given a new message mutation", ()=> {
+	m = new MockMutation("childList")
+	expect(content.isNewMessageMutation(m)).toBe(true)
+}
+)
