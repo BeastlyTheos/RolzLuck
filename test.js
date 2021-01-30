d8f8cc9094d4353b7e890b7e8d7bd767ec12235c
Ted <TheodoreCooke@trentu.ca>
2021-01-30 02:58:50 -0500

    load samples of HTMLDivElements to test against

--- test.js
+++ test.js
@@ -1,22 +1,36 @@
 const content = require("./utils")
+                const samples = require("./testSamples")
+//const fs = require("fs")
+
 
 class MockMutation {
 	constructor(type, addedNodes) {
 		this.type = type
-		            this.addedNodes = addedNodes
+		            this.addedNodes = []
+		for (const addedNode of addedNodes) {
+			var node = document.createElement("div")
+			           node.outerHTML = addedNode
+			                            this.addedNodes[this.addedNodes.length] = node
+		}
 	}
 }
 
+//get input data
+
 test(
 "isNewMessageMutation returns true when given a new message mutation", ()=> {
-	m = new MockMutation("childList", ["test"])
-	expect(content.isNewMessageMutation(m)).toBe(true)
+	for (roll of samples.rolls) {
+		m = new MockMutation("childList", [roll])
+		expect(content.isNewMessageMutation(m)).toBe(true)
+	}
 }
 )
 
 test(
 "isNewMessageMutation returns false when given anything that is not a new message mutation", ()=> {
-	m = new MockMutation("childList", [])
-	expect(content.isNewMessageMutation(m)).toBe(false)
+	for(nonRoll of samples.nonRolls) {
+		m = new MockMutation("childList", [nonRoll])
+		expect(content.isNewMessageMutation(m)).toBe(false)
+	}
 }
 )
