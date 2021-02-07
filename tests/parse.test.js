7424505f7a3b852c7ef579502d5251a1f9cdc229
Ted <TheodoreCooke@trentu.ca>
2021-02-07 05:52:22 -0500

    parser passes trivial example

--- tests/parse.test.js
+++ tests/parse.test.js
@@ -1,8 +1,10 @@
 const parse = require("../parse")
 
 test("parseDiceCode", () => {
-	var code = "d20+3"
-	var distro = parse.parseDiceCode(code)
-	console.log(code)
-	expect(distro.distro).toBe([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+	check("1", [1])
 })
+
+check = function (diceCode, expectedDistro) {
+	var distro = parse.parseDiceCode(diceCode)
+	expect(distro.distro).toEqual(expectedDistro)
+}
