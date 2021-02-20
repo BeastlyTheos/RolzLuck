5ed5228f36a2d56c0812e7fe6577c6216ee8ebe4
Ted <TheodoreCooke@trentu.ca>
2021-02-20 12:56:16 -0500

    lint background.js

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
+/* globals chrome */
 // background.js
 
 // Called when the user clicks on the browser action.
+// eslint-disable-next-line no-unused-vars
 chrome.browserAction.onClicked.addListener(function (tab) {
 	// Send a message to the active tab
 	chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
@@ -11,6 +13,7 @@ chrome.browserAction.onClicked.addListener(function (tab) {
 	})
 })
 
+// eslint-disable-next-line no-unused-vars
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 	if (request.message === "open_new_tab") {
 		chrome.tabs.create({url: request.url})
