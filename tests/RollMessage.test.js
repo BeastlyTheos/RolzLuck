dc177ad9c72c2a8d97bd7c635b65d5df7845a6ed
Ted <TheodoreCooke@trentu.ca>
2021-02-20 13:06:50 -0500

    lint RollMessage.test.js

--- tests/RollMessage.test.js
+++ tests/RollMessage.test.js
@@ -1,33 +1,34 @@
+/* globals Roll */
 const RollMessage = require("../RollMessage")
 const samples = require("./testSamples")
-Roll = RollMessage.Roll.prototype.constructor = jest.fn()
+Roll = RollMessage.Roll.prototype.constructor = jest.fn() //eslint-disable-line
 
 test("isNewMessageMutation returns true when given a new message mutation", () => {
-	for (mutation of samples.rollMutations) {
+	for (let mutation of samples.rollMutations) {
 		expect(RollMessage.isNewMessageMutation(mutation)).toBe(true)
 	}
 })
 
 test("isNewMessageMutation returns false when given anything that is not a new message mutation", () => {
-	for (mutation of samples.nonRollMutations) {
+	for (let mutation of samples.nonRollMutations) {
 		expect(RollMessage.isNewMessageMutation(mutation)).toBe(false)
 	}
 })
 
 test("parseNewMessageMutation finds correct name", () => {
-	for (mutation of samples.rollMutations) {
+	for (let mutation of samples.rollMutations) {
 		var log = new RollMessage(mutation)
 		expect(log.name).toBe("_Alfred")
 	}
 })
 
 test("RollMessage constructor", () => {
-	for (sampleRoll of samples.rolls) {
+	for (let sampleRoll of samples.rolls) {
 		Roll.mockClear()
-		log = new RollMessage(sampleRoll.mutation)
+		var log = new RollMessage(sampleRoll.mutation)
 		expect(log.name).toBe(sampleRoll.name)
 		expect(Roll.mock.calls.length).toBe(sampleRoll.diceCodes.length)
-		for (i = 0; i < sampleRoll.diceCodes.length; i++) {
+		for (let i = 0; i < sampleRoll.diceCodes.length; i++) {
 			expect(Roll.mock.calls[i][0]).toBe(sampleRoll.diceCodes[i])
 			expect(Roll.mock.calls[i][1]).toBe(sampleRoll.results[i])
 		}
