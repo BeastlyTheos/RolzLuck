3b83f8ecc43fe532438091024a4a6619c9d82358
Ted <TheodoreCooke@trentu.ca>
2021-03-13 18:00:41 -0500

    findFiles returns a list of files found

--- bundle.js
+++ bundle.js
@@ -3,17 +3,22 @@ var p = console.log
 var fs = require("fs")
 
 var findFiles = function (m) {
+	var files = []
 	for (var n in m) {
 		if (typeof m[n] === "object") {
-			findFiles(m[n])
+			files.push(...findFiles(m[n]))
 		} else {
 			if (typeof m[n] === "string" && fs.existsSync(m[n])) {
-				p(m[n] + " is a file")
+				files[files.length] = m[n]
 			}
 		}
 	}
+	return files
 }
 
 var data = fs.readFileSync("manifest.json", "utf8")
 var m = JSON.parse(data)
-findFiles(m)
+var files = findFiles(m)
+
+p(files.length)
+for (var f in files) p(files[f])
