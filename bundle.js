651f6d82b8bd02ca688ffc5da65f86a30b8538d3
Ted <TheodoreCooke@trentu.ca>
2021-03-13 17:53:15 -0500

    clean up print statements and other debug comments

--- bundle.js
+++ bundle.js
@@ -5,24 +5,15 @@ var fs = require("fs")
 var findFiles = function (m) {
 	for (var n in m) {
 		if (typeof m[n] === "object") {
-			//p(n + " is iterable")
 			findFiles(m[n])
 		} else {
-			//p(n + " is type " + typeof m[n])
-			if (typeof m[n] === "string") {
-				//p(m[n] + " is a string")
-				if (fs.existsSync(m[n])) {
-					p(m[n] + " is a file")
-				} //is a file
-				else {
-					p("not! " + m[n])
-				} //end is not file
-			} //end is a string
-		} //is not iterable
-	} //end looping
+			if (typeof m[n] === "string" && fs.existsSync(m[n])) {
+				p(m[n] + " is a file")
+			}
+		}
+	}
 }
 
 var data = fs.readFileSync("manifest.json", "utf8")
-//console.log(data);
 var m = JSON.parse(data)
 findFiles(m)
