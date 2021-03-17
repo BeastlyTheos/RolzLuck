23ef412bb1dae85ecd27b833f939700330903463
Ted <TheodoreCooke@trentu.ca>
2021-03-17 01:30:32 -0400

    install JSDom helper script to create document object
    Used slightly modified version of code found at:
    https://www.nfriedly.com/techblog/2013/02/automatically-unit-testing-client-side-javascript-with-jasmine-and-node-js/

---
+++ spec/helpers/jasmine.helper.js
@@ -0,0 +1,17 @@
+/* globals global */
+var jsdom = require("jsdom")
+
+// eslint-disable-next-line no-global-assign
+window = new jsdom.JSDOM(
+	'<html><head></head><body><div id="rondavu_container"></div></body></html>'
+).window
+
+if (Object.keys(window).length === 0) {
+	// this hapens if contextify, one of jsdom's dependencies doesn't install correctly
+	// (it installs different code depending on the OS, so it cannot get checked in.);
+	throw "jsdom failed to create a usable environment, try uninstalling and reinstalling it"
+}
+
+global.window = window
+
+global.document = window.document
